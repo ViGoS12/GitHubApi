d04c653b564c08716138b8ecc22d1f084806cb0b
Kirill <kirisakov4@yandex.ru>
2022-10-29 21:33:20 +0300

    feat: Added new Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,9 @@
+import Repository from '../pages/Repository'
 import Home from './../pages/Home'
+import User from './../pages/User'
 
-export const publicRoutes = [{ path: '/', element: <Home /> }]
+export const publicRoutes = [
+  { path: '/', element: <Home /> },
+  { path: ':userName/repos', element: <User /> },
+  { path: ':userName/repos/:repoName', element: <Repository /> },
+]
